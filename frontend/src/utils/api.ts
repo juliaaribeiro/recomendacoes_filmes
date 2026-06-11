be759fb9389a1b15b4873e46618c08b6108cb228
Bianca <biancafeliciano@outlook.com>
2026-06-11 19:57:56 -0300

    config: pronto para deploy

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,3 +1,3 @@
-export const TMDB_API_KEY = 'e85b853a307c5d0041007f7df100d1b9'
+export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
-export const BACKEND_URL = 'http://localhost:8000'
+export const BACKEND_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
